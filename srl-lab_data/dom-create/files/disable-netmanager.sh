dee14ba6ace7fd84e51ffa4b518891276ae645fe
root <root@baremetal.vim.nokialab.net>
2021-06-03 16:43:03 -0500

    deleted:    deploy-kvm-instances.yml 	deleted:    srl-lab_data/dom-create/defaults/main.yml 	deleted:    srl-lab_data/dom-create/files/disable-netmanager.sh 	deleted:    srl-lab_data/dom-create/tasks/1_check_hypervisor.yml 	deleted:    srl-lab_data/dom-create/tasks/2.1_prep_qcow2_image.yml 	deleted:    srl-lab_data/dom-create/tasks/2_copy_and_resize_qcow.yml 	deleted:    srl-lab_data/dom-create/tasks/3_cloud_init_files.yml 	deleted:    srl-lab_data/dom-create/tasks/4_guestfish_system_files.yml 	deleted:    srl-lab_data/dom-create/tasks/5_define_and_create_vm.yml 	deleted:    srl-lab_data/dom-create/tasks/6_cloud_init_files_remove.yml 	deleted:    srl-lab_data/dom-create/tasks/main.yml 	deleted:    srl-lab_data/dom-create/templates/authorized_keys.j2 	deleted:    srl-lab_data/dom-create/templates/ifcfg-eth.j2 	deleted:    srl-lab_data/dom-create/templates/meta-data.j2 	deleted:    srl-lab_data/dom-create/templates/network.j2 	deleted:    srl-lab_data/dom-create/templates/nodes.xml.j2 	deleted:    srl-lab_data/dom-create/templates/resolv.conf.j2 	deleted:    srl-lab_data/dom-create/templates/temp-ifcfg-eth.j2 	deleted:    srl-lab_data/dom-create/templates/user-data.j2 	deleted:    srl-lab_data/dom-create/vars/RedHat.yml

--- srl-lab_data/dom-create/files/disable-netmanager.sh
+++
@@ -1,4 +0,0 @@
-guestfish -a $1 -i ln-sf /dev/null /etc/systemd/system/NetworkManager.service
-guestfish -a $1 -i ln-sf /dev/null /etc/systemd/system/NetworkManager-dispatcher.service
-guestfish -a $1 -i ln-sf /dev/null /etc/systemd/system/NetworkManager-wait-online.service
-guestfish -a $1 -i upload /tmp/temp-ifcfg-eth /etc/sysconfig/network-scripts/ifcfg-eth0
