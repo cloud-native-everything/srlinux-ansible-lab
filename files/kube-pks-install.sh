31cf9fc14dc6adc462078a8a56917f5b7e1dfc89
root <root@baremetal.vim.nokialab.net>
2021-06-25 17:58:20 -0500

    adding squid server

---
+++ files/kube-pks-install.sh
@@ -0,0 +1,28 @@
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# Enable iptables bridge routing
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+
+sudo yum -y clean all
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo yum -y clean all
+systemctl restart kubelet
+sudo systemctl enable --now kubelet
