b9ab71da20c4e7e2057945abaf0808b8f6494bd5
root <root@baremetal.vim.nokialab.net>
2021-08-30 08:30:11 -0500

    backing up

--- files/fss-net-qemu-container.sh
+++ files/fss-net-qemu-container.sh
@@ -1,7 +1,7 @@
 rm -rf /var/run/netns/qemu-$1
 pid=$(docker inspect -f '{{.State.Pid}}' qemu-$1)
 sudo ln -sf /proc/$pid/ns/net /var/run/netns/qemu-$1
-br_srl=$(brctl show | grep 'br-' | grep 'veth' | awk '{print $1}')
+br_srl="fss-bridge"
 
 ip netns exec qemu-$1 ip link set eth0 down
 ip netns exec qemu-$1 ip link delete eth0
@@ -14,3 +14,8 @@ sudo brctl addif $br_srl $1-veth1
 
 sudo ip netns exec qemu-$1 ip link set eth0 up
 sudo ip link set $1-veth1 up
+
+sudo ip link set $1-veth1 mtu 9000
+sudo ip netns exec qemu-$1 ip link set eth0 mtu 9000
+sudo ip netns exec qemu-$1 ip link set tap0 mtu 9000
+sudo ip netns exec qemu-$1 ip link set tap1 mtu 9000
